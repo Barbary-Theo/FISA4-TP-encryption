a288e3e750316d0f150c6cfcf669cf9a210d3434
Barbary-Theo <theo.barbary25@gmail.com>
2022-10-12 15:11:38 +0200

    init project

---
+++ src/tasks/Task2.java
@@ -0,0 +1,25 @@
+package tasks;
+
+import java.io.File;
+
+public class Task2 {
+	
+	public static void main(String[] args) throws Exception {
+		
+	}
+			
+	public static byte[] computeMD5Hashes(File fileToHash) throws Exception {
+		return null;
+	}
+	
+	public static byte[] computeSHA256Hashes(File fileToHash) throws Exception {
+		return null;
+	}
+	
+	/*use HMAC-SHA256*/
+	public static byte[] computeHMAC(File fileToHashMac) throws Exception {
+		return null;
+	}
+
+}
+
---
+++ src/tasks/Task3.java
@@ -0,0 +1,26 @@
+package tasks;
+
+import java.security.KeyPair;
+
+public class Task3 {
+	
+	public static void main(String[] args) throws Exception {
+		
+	}
+	
+	public static KeyPair aliceGenerateDHKeypair() throws Exception{
+		 return null;
+	}
+	
+	public static KeyPair bobGenerateDHKeypair(byte[] AlicePublicKeyBytes) throws Exception{
+		return null;
+	}
+	
+	public static byte[] aliceGenerateSharedSecret(byte[] BobPublicKeyBytes, KeyPair AliceKeys) throws Exception {
+		return null;
+	}
+	
+	public static byte[] bobGenerateSharedSecret(byte[] AlicePublicKeyBytes, KeyPair BobKeys) throws Exception {
+		return null;
+	}
+}
